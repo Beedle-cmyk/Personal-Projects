ccc453a35ca61fd73ede4a42364fb99ecd4e96ef
beedle-cmyk <sami.ibrahim088@gmail.com>
2023-12-06 13:10:16 -0800

    Final commit

--- int2string/int2string/int2string.cpp
+++ int2string/int2string/int2string.cpp
@@ -22,35 +22,9 @@ char* int2str(int integer) {
 
 int main()
 {
-	// std::cout << "Hello World!\n";
-	// Testing commiting a change
-	// Commiting a second change
-	// Final commit practice
-
 	char* returnstring = int2str(123);
+	// Remove used memory at location once done with it
 	delete[] returnstring;
-
-	// int foo = 103;
-	// int moduloValue = 0;
-
-	/*while (foo > 0)
-	{
-		moduloValue = foo % 10;
-		foo /= 10;
-	}*/
-
-	// Pretend we have the starting number 9
-	// binary: 1001
-	// If 9 is left-shifted by 1 bit, you have a new number:
-	// 10010, or 18
-	// Now pretend you have a 32 bit integer
-	//  You want to get each bit off the number
-	// and convert that bit to a character.
-	// E.g:
-	// 00110011 10111010 10000001 10101010
-	// If you right-shift this number by 1, all the bits move over by 1, and the most significant bit is 0
-	// 00011001 11011101 01000000 11010101
-	// If you can capture the bit that was shifted off, it's simple to figure out if it's a string '1' or '0'
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
