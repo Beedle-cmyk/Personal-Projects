d52f24ed315d9db50e8ce512ff249da1e8ca8a40
beedle-cmyk <sami.ibrahim088@gmail.com>
2024-02-05 18:19:43 -0800

    Added functionality

--- bitmask/bitmask.cpp
+++ bitmask/bitmask.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 using namespace std;
+#define settingIsNightTime = 0x1
+#define settingIsRain      = 0x2
+#define settingIs6PM       = 0x04
+#define settingIsAreaClear = 0x08
+#define settingIsTornado   = 0x10
+#define settingIsAlive     = 0x20
+#define settingIsAwake     = 0x40
+#define settingIsHappy     = 0x80
+
 
 /*
 1) pass 2 parameters: Mask and Value
@@ -7,6 +16,8 @@ using namespace std;
 3) compare each bit Between the value and mask using suitable operator 
 4) Return the Resultant Value
 */
+
+// Checking for 1 setting, C++ #define hex
 
 
 void check(int Mask, int Value)
@@ -30,6 +41,12 @@ void check(int Mask, int Value)
 		Mask = Mask/2;
 
 	}
+}
+
+
+void printSettings(int Settings)
+{
+
 }
 
 
