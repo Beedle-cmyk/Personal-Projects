4475b13d33123863de5309752dfb0e5494783311
beedle-cmyk <sami.ibrahim088@gmail.com>
2024-02-13 17:35:32 -0800

    comments made

--- bitmask/bitmask.cpp
+++ bitmask/bitmask.cpp
@@ -9,6 +9,17 @@ using namespace std;
 #define settingIsAwake      0x40
 #define settingIsHappy      0x80
 
+/*
+This function should receive an int which contains many settings and should print one statement per setting, indicating whether or not the setting is set. E.g., one print line per setting:
+#define settingIsNightTime = 0x1
+#define settingIsRain = 0x2
+#define settingIs6PM = 0x04
+#define settingIsAreaClear = 0x08
+#define settingIsTornado = 0x10
+#define settingIsAlive = 0x20
+#define settingIsAwake = 0x40
+#define settingIsHappy = 0x80
+*/
 
 void printSettings(int Settings)
 {
