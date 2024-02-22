cb0a5eeca8dead68d7f2a7fdd0728afc6f71e222
beedle-cmyk <sami.ibrahim088@gmail.com>
2024-02-22 15:25:10 -0800

    C

--- bitmask/bitmask.cpp
+++ bitmask/bitmask.cpp
@@ -21,46 +21,79 @@ This function should receive an int which contains many settings and should prin
 #define settingIsHappy = 0x80
 */
 
-/*
-Check the provided int for 8 settings, return an integer with all the true settings set to 1
-E.g.: when bitmaskedSettings == 0xC0, return value is 0xC0
-when bitmaskedSettings == 0x80AC00C0, return value is 0xC0
-compare in test function with resultant value 
-test number with 0th and 1st bit set with other stuff
-Expand each nibble
-*/
-
 void printSettings(int Settings)
 {
 	int Mask = 7;
 
 	int Result = Mask & Settings;
-	int counter = 1;
+	int bitValue = 1;
 
-	for (int i = 0; i < Result; i++)
+	for (int i = 0; i < 8; i++)
 	{
-		if (counter & Result)
+		if (bitValue & Result)
 		{
-			cout << counter << endl;
+			cout << bitValue << endl;
 		}
-		counter*=2;
+
+		else
+		{
+			cout << 0 << endl;
+		}
+
+		bitValue *= 2;
 	}
 
 }
 
+/*
+Check the provided int for 8 settings, return an integer with all the true settings set to 1
+E.g.: when bitmaskedSettings == 0xC0, return value is 0xC0
+when bitmaskedSettings == 0x80AC00C0, return value is 0xC0
+compare in test function with resultant value
+test number with 0th and 1st bit set with other stuff
+Expand each nibble
+*/
+
 int getTrueSettings(int bitmaskedSettings)
 {
+	int bitValue = 1;
+	int newValue = 0;
 
-	int Mask = 7;
+	for (int i = 0; i < 8; i++)
+	{
+		if (bitValue & bitmaskedSettings)
+		{
+			newValue += bitValue;
+		}
+		bitValue *= 2;
+	}
 
-	
-	return 0;
+	return newValue;
+}
+
+void TestFunction(int Test, int expectedValue)
+{
+	int actualValue = getTrueSettings(Test);
+
+		if (actualValue == expectedValue)
+		{
+			std::cout << "Pass " << actualValue << " is equal to " << expectedValue << std::endl;
+		}
+		else
+		{
+			std::cout << "Error " << actualValue << " is not equal to " << expectedValue << std::endl;
+		}
 
 }
 
 
-int main() 
+int main()
 {
-	printSettings();
+
+	TestFunction(0xC0, 0xC0);
+	TestFunction(0x80AC00C0, 0xC0);
+	TestFunction(0xFFFF, 255);
+	TestFunction(8, 8);
+
 	return 0;
 }
