3ab09af2bcf3cf7709c1aaa402bb5f128f0c3094
beedle-cmyk <sami.ibrahim088@gmail.com>
2024-02-27 13:56:57 -0800

    Blank space compatibility

--- string2int/string2int/string2int.cpp
+++ string2int/string2int/string2int.cpp
@@ -1,9 +1,50 @@
 #include <iostream>
-#define zeroInASCII 48
-#define minusSignInASCII 45
+#define zeroInASCII       48
+#define minusSignInASCII  45
+#define blankSpaceInASCII 32
 
 int str2int(const char* pInputString)
 {
+	/*
+	//decreasing value as to not start at the null pointer (therefore start at the next value over)
+	int currentStringSize = strlen(pInputString) - 1;
+	bool isNegative = false;
+	int index = 0;
+
+	if (pInputString[0] == minusSignInASCII)
+	{
+		index++;
+		isNegative = true;
+	}
+
+
+	int coefficient = pow(10, currentStringSize-index);
+	int OutputInteger = 0;
+
+	for (index; index <= currentStringSize; index++)
+	{
+		if (pInputString[index] == blankSpaceInASCII)
+		{
+			coefficient /= 10;
+			index++;
+		}
+		else
+		{
+			int temp = coefficient * (pInputString[index] - zeroInASCII);
+			OutputInteger += temp;
+			coefficient /= 10;
+		}
+	}
+
+	if (isNegative == true)
+	{
+		OutputInteger *= -1;
+	}
+
+	return OutputInteger;
+	*/
+
+	
 	//decreasing value as to not start at the null pointer (therefore start at the next value over)
 	int currentStringPosition = strlen(pInputString) - 1;
 	int isNegativeNumber = false;
@@ -16,14 +57,23 @@ int str2int(const char* pInputString)
 	//setting up a coefficient change from tenths to hundredths, etc
 	int coefficient = 1;
 	int OutputInteger = 0;
-
+	
+	//The position string starts at the final position and decreases until it reaches the first position
+	//If this first position is minusSignInASCII it skips it by stopping when the position is 0 (First position)
 	while (currentStringPosition > -1 + isNegativeNumber)
 	{
-		//decreasing by 48 to obtain integer ASCII value
-		int temp = coefficient * (pInputString[currentStringPosition] - zeroInASCII);
-		OutputInteger += temp;
-		coefficient *= 10;
-		currentStringPosition--;
+		if (pInputString[currentStringPosition] == blankSpaceInASCII)
+		{
+			currentStringPosition--;
+		}
+		else
+		{
+			//decreasing by 48 to obtain integer ASCII value
+			int temp = coefficient * (pInputString[currentStringPosition] - zeroInASCII);
+			OutputInteger += temp;
+			coefficient *= 10;
+			currentStringPosition--;
+		}
 	}
 
 
@@ -33,6 +83,7 @@ int str2int(const char* pInputString)
 
 	}
 	return OutputInteger;
+	
 }
 
 void teststr2int(const char* Test, int expectedInteger) 
@@ -55,6 +106,9 @@ int main()
 	teststr2int("123", 123);
 	teststr2int("-123", -123);
 	teststr2int("0", 0);
+	teststr2int(" 0   ", 0);
+	teststr2int("1               2                 3", 123);
+	teststr2int("-12                 4", -124);
 
 
 	return 0;
