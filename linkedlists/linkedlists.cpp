27da74efbcb87b0b8481fe1f272ec3de7115aa61
beedle-cmyk <sami.ibrahim088@gmail.com>
2024-03-01 11:51:34 -0800

    Start

---
+++ linkedlists/linkedlists.cpp
@@ -0,0 +1,26 @@
+/*
+Create a header file that defines a class called linked list
+Learn what a header file is
+header is the structure
+
+
+node class contains a pointer to the next node and a payload
+int payload node* next
+
+Within your class define these functions:
+
+Add node(1 paramter) node*
+
+int Count returns the number of nodes in a list
+
+remove() take away a node
+
+clear /Empties l
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
