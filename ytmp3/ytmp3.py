98953c55c6cb69c12e0eae6af201456191468e6c
beedle-cmyk <sami.ibrahim088@gmail.com>
2024-05-25 11:24:18 -0700

    A start on Playlists

--- ytmp3/ytmp3.py
+++ ytmp3/ytmp3.py
@@ -1,19 +1,27 @@
 from pytube import YouTube
+from pytube import Playlist
 
 #https://www.youtube.com/watch?v=gjSqjnuPGJA
 
 def Download(ytLink):
-    #Object instantiation
-    youtube = YouTube(ytLink)
-    try:
-       #Obtaining the stream with the highest abr (average bit rate)
-       #The highest abr always contains the itag = 251
-       stream = youtube.streams.get_by_itag(251)
-       stream.download("C:\Github\Personal-Projects\ytmp3\YTdownloads",youtube.title+".mp3")
-       print("Pass")
-    except:
-        print("Error")
+    
+    #Checking if the link is a playlist link
+    if "&list=" in ytLink:
+        playlist = Playlist(ytLink)
 
+    else:
+        #Object instantiation
+        youtube = YouTube(ytLink)
+        try:
+            #Obtaining the stream with the highest abr (average bit rate)
+            #The highest abr always contains the itag = 251
+            stream = youtube.streams.get_by_itag(251)
+            print(f'Downloading: {youtube.title}.....')
+            stream.download("C:\Github\Personal-Projects\ytmp3\YTdownloads",youtube.title+".mp3")
+            print("Pass")
+        except:
+            print("Error")
 
-ytLink = input("Paste YouTube link:")
-Download(ytLink)
+while True:
+    ytLink = input("Paste YouTube link/Playlist link:")
+    Download(ytLink)
