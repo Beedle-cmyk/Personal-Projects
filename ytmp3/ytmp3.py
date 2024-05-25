44ad1a7fba8a5ed5881b819b14758e94b7aae280
beedle-cmyk <sami.ibrahim088@gmail.com>
2024-05-25 13:18:40 -0700

    C

--- ytmp3/ytmp3.py
+++ ytmp3/ytmp3.py
@@ -1,24 +1,46 @@
 from pytube import YouTube
 from pytube import Playlist
+from pytube.exceptions import VideoUnavailable
 
-#https://www.youtube.com/watch?v=gjSqjnuPGJA
+def titleStringTrim(Object):
+    newString = Object.title
+    newString = newString.replace('"',"")
+    newString = newString.replace('\\', '')
+    newString = newString.replace('/', '')
+    return newString
+
+def doPlaylist(ytLink):
+        
+        playlist = Playlist(ytLink)
+        for video in playlist.videos:
+            try:
+                #Obtaining the stream with the highest abr (average bit rate)
+                #The highest abr always contains the itag = 251  
+                stream = video.streams.get_by_itag(251)
+                stream.download("C:\Github\Personal-Projects\ytmp3\YTdownloads\Playlist",titleStringTrim(video)+".mp3")
+                print(f'Downloaded: {video.title}')
+            except VideoUnavailable:
+                print(f"Video {video.title} is unavaialable, skipping.")
+            except:
+                print("Error")
+    
 
 def Download(ytLink):
-    
-    #Checking if the link is a playlist link
-    if "&list=" in ytLink:
-        playlist = Playlist(ytLink)
 
+    #Check if link is a playlist link
+    if "&list=" in ytLink:
+        doPlaylist(ytLink)
     else:
-        #Object instantiation
         youtube = YouTube(ytLink)
         try:
             #Obtaining the stream with the highest abr (average bit rate)
-            #The highest abr always contains the itag = 251
+            #The highest abr for audio always contains the itag = 251
             stream = youtube.streams.get_by_itag(251)
-            print(f'Downloading: {youtube.title}.....')
-            stream.download("C:\Github\Personal-Projects\ytmp3\YTdownloads",youtube.title+".mp3")
-            print("Pass")
+            print(f"Downloading: {youtube.title}.....")
+            stream.download("C:\Github\Personal-Projects\ytmp3\YTdownloads",titleStringTrim(youtube)+".mp3")
+            print(f"Downloaded: {youtube.title}")
+        except VideoUnavailable:
+            print(f"Video {youtube.title} is unavaialable.")
         except:
             print("Error")
 
