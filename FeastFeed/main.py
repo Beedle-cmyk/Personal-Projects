c73a5227ba67f19954fd6f3664b7f903b1eaf955
beedle-cmyk <sami.ibrahim088@gmail.com>
2025-09-18 20:32:22 -0400

    html getter working

--- FeastFeed/main.py
+++ FeastFeed/main.py
@@ -1,1 +1,7 @@
-print("Hello")
+from bs4 import BeautifulSoup 
+import requests
+
+url = "https://huskylink.washington.edu/event/11411956"
+result = requests.get(url)
+doc = BeautifulSoup(result.text, 'html.parser')
+print(doc.prettify())
