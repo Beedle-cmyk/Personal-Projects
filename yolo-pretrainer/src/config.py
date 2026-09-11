39cc201c9b7bb07679fda8219608faa9dfccea48
beedle-cmyk <sami.ibrahim088@gmail.com>
2026-09-11 17:36:25 -0400

    C

--- yolo-pretrainer/src/config.py
+++ yolo-pretrainer/src/config.py
@@ -11,45 +11,61 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# ---- Directory paths ----
-#BASE_DIRECTORY = Path(r"C:\borescope-yolo-training")
-BASE_DIRECTORY = Path(r"C:\Personal-Projects\yolo-pretrainer")
-
-# Please Set this to your current working project e.g. C:\borescope-yolo-training\projects\yolo26_v1.7_seg_900
-CURRENT_WORKING_PROJECT_DIRECTORY = Path(r"C:\Personal-Projects\yolo-pretrainer\projects\yolo26_v2.2_seg_234")
+# ------------------------------------------- Directory paths ---------------------------------------------------------- #
+# PLEASE FILL THIS IN
+BASE_DIRECTORY = Path(r"FILL THIS IN!")  # Where your repository is e.g. C:/borescope-yolo-training
 
 PROJECT_DIRECTORY = BASE_DIRECTORY / r"projects"
 DATA_DIRECTORY = BASE_DIRECTORY / r"data/images"
 CFG_DIRECTORY = BASE_DIRECTORY / r"src/cfg"
 
-# Provide a label
+# Provide a label studio exported json file e.g. yolo26_v2.1_seg_234.json
 LABELS_JSON_FILE = r"C:\Personal-Projects\yolo-pretrainer\projects\yolo26_v2.1_seg_234\labelstudio\yolo26_v2.1_seg_234.json"
 
+# Create a project using setup_project.py first before setting this
+CURRENT_WORKING_PROJECT_DIRECTORY = PROJECT_DIRECTORY / r"FILL THIS IN!"  #e.g. yolo26_v1.2_seg_2808
+# ---------------------------------------------------------------------------------------------------------------------- #
+
+
+
+# ------------------------------------------- environ (private data) ---------------------------------------------------------- #
 API_KEY = os.environ["API_KEY"]
 LABEL_STUDIO_EXE = os.environ["LABEL_STUDIO_PATH"]
+# ----------------------------------------------------------------------------------------------------------------------------- #
 
-# ---- Labelling Interface Templates (Copy-pasted from label studio) ----
-LABEL_CONFIG_BOX = CFG_DIRECTORY / r"label_config_box.xml"
+
+
+# --------------------------Labelling Interface Templates (Copy-pasted from label studio) --------------------- #
+#LABEL_CONFIG_BOX = CFG_DIRECTORY / r"label_config_box.xml"
 LABEL_CONFIG_SEG = CFG_DIRECTORY / r"label_config_seg.xml"
 
-# SET LABEL_CONFIG for either segmentation or box
+# SET LABEL_CONFIG for either segmentation or box; Default is segmentation
 LABEL_CONFIG = LABEL_CONFIG_SEG
 LABEL_CONFIG_RAW = Path(LABEL_CONFIG).read_text(encoding="utf-8")
+# ------------------------------------------------------------------------------------------------------------- #
 
-# Enabling this will auto write the tuner recommended parameters from 'best_hyperparameters.yaml' to the current project's 'args.yaml'
-# This is used in tune.py
-UPDATE_ARGS_YAML_WITH_TUNED_ONES = False
+
+
+# --------------------------------------Training Args Configs-------------------------------------------------- #
 ARGS_YAML_CONFIG = CFG_DIRECTORY / r"args.yaml"
 TUNE_ARGS_YAML_CONFIG = CFG_DIRECTORY / r"tune_args.yaml"
 
-# Path to model if you would like to resume training from a checkpoint
+# Enabling this will auto write the tuner recommended parameters from 'best_hyperparameters.yaml' to the current project's 'args.yaml' in tune.py
+UPDATE_ARGS_YAML_WITH_TUNED_ONES = False
+
+# Path to model if you would like to resume training from a checkpoint e.g. 
 RESUME_MODEL = None
 
 # trainer.py maximum amount of times training attempts to resume
 MAX_RETRIES = 3
+# ------------------------------------------------------------------------------------------------------------- #
 
+
+
+# --------------------------------------Prediction/Prelabelling Args Configs-------------------------------------------------- #
 # Model path e.g. to best.pt used for predictions or prelabelling
-MODEL_FOR_PREDICTIONS = r"C:\Personal-Projects\yolo-pretrainer\projects\yolo26_v1.1_seg_279\runs\train_yolo26s-seg_imgsz_640_epochs_100\weights\best_yolo26s-seg_imgsz_640_epochs_100.pt"
+MODEL_FOR_PREDICTIONS = r"FILL THIS IN!"
 MIN_CONF = 0.25
 MAX_CONF = 1.0
-IMAGES_TO_PREDICT = r"C:\Personal-Projects\yolo-pretrainer\projects\yolo26_v2.1_seg_234\original_data\images"
+IMAGES_TO_PREDICT = r"FILL THIS IN!"
+# ---------------------------------------------------------------------------------------------------------------------------- #
